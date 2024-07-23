8d76930229223b2f320c482ce77095eed3b192a3
Khyeji <khjji0502@gmail.com>
2024-07-23 15:36:50 +0900

    project init

---
+++ FirebaseServerEx/FirebaseServerEx/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FirebaseServerEx
+//
+//  Created by heyji on 2024/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
